d189dadf489652a2e23339ee00c80910e0ba0a6f
Jonathan Manner <Jonathanmanner@hotmail.com>
2021-10-14 21:57:26 +0200

    minor changes

--- project-local-winds/localWinds.js
+++ project-local-winds/localWinds.js
@@ -7,8 +7,10 @@
 //windDataHolder["timeSeries"][0]["parameters"]["11"]["values"]["0"] === air pressure (hPa)
 
 const requestBtn = document.getElementById("current-wind-btn");
-const apiOutputOne = document.getElementById("json-output-1");
-const apiOutputTwo = document.getElementById("json-output-2");
+const apiOutputWindDir = document.getElementById("json-output-1");
+const apiOutputWindSpeed = document.getElementById("json-output-2");
+const apiOutputTemp = document.getElementById("json-output-3");
+const apiOutputAirPressure = document.getElementById("json-output-4");
 const fetchSpinner = document.getElementById("fetch-spinner");
 const windArrow = document.getElementById("arrowAnim");
 let windDataHolder;
@@ -47,8 +49,10 @@ const fetchData = async () => {
       "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18/lat/59/data.json"
     );
     windDataHolder = await response.json(); //extract JSON from the http response
-    apiOutputOne.innerHTML = `Wind direction: ${windDataHolder["timeSeries"][0]["parameters"]["13"]["values"]["0"]} degrees at ${windDataHolder["timeSeries"][0]["parameters"]["14"]["values"]["0"]} m/s`;
-    apiOutputTwo.innerHTML = `Outdoor temperature: ${windDataHolder["timeSeries"][0]["parameters"]["10"]["values"]["0"]} degrees. Air pressure: ${windDataHolder["timeSeries"][0]["parameters"]["11"]["values"]["0"]} hPa`;
+    apiOutputWindDir.innerHTML = `Wind direction: ${windDataHolder["timeSeries"][0]["parameters"]["13"]["values"]["0"]} degrees.`;
+    apiOutputWindSpeed.innerHTML = `Wind speed: ${windDataHolder["timeSeries"][0]["parameters"]["14"]["values"]["0"]} m/s.`;
+    apiOutputTemp.innerHTML = `Outdoor temperature: ${windDataHolder["timeSeries"][0]["parameters"]["10"]["values"]["0"]} degrees.`;
+    apiOutputAirPressure.innerHTML = `Air pressure: ${windDataHolder["timeSeries"][0]["parameters"]["11"]["values"]["0"]} hPa`;
     animateArrow();
     clearTimeout(time);
     fetchSpinner.style.visibility = "hidden";
