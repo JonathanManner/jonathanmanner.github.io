f0ffb2a97d22d40a9b9df840b677e068167d4fdf
Jonathan Manner <Jonathanmanner@hotmail.com>
2021-10-27 08:45:16 +0200

    added locations

--- project-local-winds/localWinds.js
+++ project-local-winds/localWinds.js
@@ -10,23 +10,28 @@
 
 const lisoRequestBtn = document.getElementById("liso-wind-btn");
 const falunRequestBtn = document.getElementById("falun-wind-btn");
+const savsjoRequestBtn = document.getElementById("savsjo-wind-btn");
 const windDirOutput = document.getElementById("json-output-1");
 const windSpeedOutput = document.getElementById("json-output-2");
 const tempOutput = document.getElementById("json-output-3");
 const airPressureOutput = document.getElementById("json-output-4");
 const WindDirFalun = document.getElementById("json-output-5");
 const WindSpeedFalun = document.getElementById("json-output-6");
-const TempFalun = document.getElementById("json-output-7");
+const tempFalun = document.getElementById("json-output-7");
 const airPressureFalun = document.getElementById("json-output-8");
+const windDirSavsjo = document.getElementById("json-output-9");
+const windSpeedSavsjo = document.getElementById("json-output-10");
+const tempSavsjo = document.getElementById("json-output-11");
+const airPressureSavsjo = document.getElementById("json-output-12");
 const fetchSpinner = document.getElementById("fetch-spinner");
 const lisoWindArrow = document.getElementById("lisoArrowAnim");
 const falunWindArrow = document.getElementById("falunArrowAnim");
+const savsjoWindArrow = document.getElementById("savsjoArrowAnim");
 const lisoUrl = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/17.45/lat/58.55/data.json";
 const falunUrl = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/15.37/lat/60.36/data.json";
 const savsjoUrl = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/14.39/lat/57.24/data.json";
 
 let isStarted = false;
-// const latestForecastRoot = '${tempWeatherData["timeSeries"][0]["parameters"]';
 
 const animateArrow = (location, formattedWeatherData) => {
   //add +90 to wind degrees to compensate for animation :)
@@ -41,6 +46,11 @@ const animateArrow = (location, formattedWeatherData) => {
       formattedWeatherData["wd"]["values"][0] + 270
     }deg)`;
     falunWindArrow.style.visibility = "visible";
+  } else if (location === 'savsjo') {
+    savsjoWindArrow.style.transform = `rotate(${
+      formattedWeatherData["wd"]["values"][0] + 270
+    }deg)`;
+    savsjoWindArrow.style.visibility = "visible";
   }
 };
 
@@ -56,7 +66,7 @@ const throttleReqest = () => {
   isStarted = true;
   setTimeout(() => {
     isStarted = false;
-  }, 2000); //Mitigate unnecessary requests.
+  }, 400); //Mitigate unnecessary requests.
 };
 
 const renderResponse = (location, latestForecastParameters) => {
@@ -68,12 +78,15 @@ const renderResponse = (location, latestForecastParameters) => {
   } else if (location === 'falun') {
   WindDirFalun.innerHTML = `Wind direction: ${latestForecastParameters["wd"]["values"][0]} degrees.`;
   WindSpeedFalun.innerHTML = `Wind speed: ${latestForecastParameters["ws"]["values"][0]} m/s.`;
-  TempFalun.innerHTML = `Outdoor temperature: ${latestForecastParameters["t"]["values"][0]} degrees.`;
+  tempFalun.innerHTML = `Outdoor temperature: ${latestForecastParameters["t"]["values"][0]} degrees.`;
   airPressureFalun.innerHTML = `Air pressure: ${latestForecastParameters["msl"]["values"][0]} hPa`;
+  } else if (location === 'savsjo') {
+  windDirSavsjo.innerHTML = `Wind direction: ${latestForecastParameters["wd"]["values"][0]} degrees.`;
+  windSpeedSavsjo.innerHTML = `Wind speed: ${latestForecastParameters["ws"]["values"][0]} m/s.`;
+  tempSavsjo.innerHTML = `Outdoor temperature: ${latestForecastParameters["t"]["values"][0]} degrees.`;
+  airPressureSavsjo.innerHTML = `Air pressure: ${latestForecastParameters["msl"]["values"][0]} hPa`;
   }
 };
-
-// let formattedWeatherData;
 
 // Fetch weather data from SMHI
 const fetchData = async (url, location) => {
@@ -109,4 +122,8 @@ let lisoBtnClick = lisoRequestBtn.addEventListener("click", () => {
 
 let falunBtnClick = falunRequestBtn.addEventListener("click", () => {
   return fetchData(falunUrl, 'falun');
+});
+
+let savsjoBtnClick = savsjoRequestBtn.addEventListener("click", () => {
+  return fetchData(savsjoUrl, 'savsjo');
 });
